fa51cb22d6d637e2939f323bc33ab511b2de9d8a
kozraf <kozraf@gmail.com>
2023-06-12 23:27:34 +0100

    experimenting with NFS server setup

--- NFS_shared_storage/nfs-server-setup.sh
+++ NFS_shared_storage/nfs-server-setup.sh
@@ -10,7 +10,7 @@ sudo chown nobody:nogroup /srv/nfs
 sudo chmod 777 /srv/nfs
 
 # Configure NFS exports so node2 and node3 can access it
-echo '/srv/nfs/k8s_pv_jenkins 192.168.89.142(rw,sync,no_subtree_check) 192.168.89.143(rw,sync,no_subtree_check)' | sudo tee -a /etc/exports
+sudo echo '/srv/nfs/k8s_pv_jenkins 192.168.89.142(rw,sync,no_subtree_check) 192.168.89.143(rw,sync,no_subtree_check)' | sudo tee -a /etc/exports
 
 # Restart NFS server and enable on boot
 sudo systemctl restart nfs-kernel-server
