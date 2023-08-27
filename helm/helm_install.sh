a89ac2492c6dd7f98d1ca389810c12974da1e6e0
kozraf <kozraf@gmail.com>
2023-08-27 22:08:46 +0100

    Organising apps in RafK8clstr folder

--- helm/helm_install.sh
+++ helm/helm_install.sh
@@ -1,5 +1,4 @@
-mkdir /home/vagrant/helm
-curl -fsSL -o /home/vagrant/helm/get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 /home/vagrant/helm/get_helm.sh
-/home/vagrant/helm/get_helm.sh
+curl -fsSL -o /home/vagrant/RafK8clstr/helm/get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 /home/vagrant/RafK8clstr/helm/get_helm.sh
+/home/vagrant/RafK8clstr/helm/get_helm.sh
 helm version
