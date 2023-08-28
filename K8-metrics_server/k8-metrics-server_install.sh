b4af8a2c4837c3e59324eda6fc400332b9a09480
kozraf <kozraf@gmail.com>
2023-08-28 20:05:19 +0100

    fixing cert approval for k8-metrics-server_install.sh

--- K8-metrics_server/k8-metrics-server_install.sh
+++ K8-metrics_server/k8-metrics-server_install.sh
@@ -1,6 +1,8 @@
 #https://blog.zespre.com/deploying-metrics-server-on-kubernetes-cluster-installed-with-kubeadm.html
+echo -e "--------"
+echo -e "Applying pending certs"
 kubectl get csr | grep Pending | awk '{print $1}' | xargs -I {} kubectl certificate approve {}
-#kubectl get csr | grep Pending | awk '{print $1}' | xargs -I {} kubectl certificate approve {}
+kubectl get csr
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm upgrade --install metrics-server metrics-server/metrics-server
 
