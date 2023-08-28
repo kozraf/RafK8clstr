4ed1c61d26917d22188f9bd904d9980b640c909d
kozraf <kozraf@gmail.com>
2023-08-28 10:11:20 +0100

    sorting out k8-metrics-server_install.sh to solve bug with kubectl get cert

--- K8-metrics_server/k8-metrics-server_install.sh
+++ K8-metrics_server/k8-metrics-server_install.sh
@@ -1,5 +1,5 @@
 #https://blog.zespre.com/deploying-metrics-server-on-kubernetes-cluster-installed-with-kubeadm.html
-kubectl get csr | grep Pending | awk '{print $1}' | xargs -I {} kubectl certificate approve {}
+sudo kubectl get csr | grep Pending | awk '{print $1}' | xargs -I {} kubectl certificate approve {}
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm upgrade --install metrics-server metrics-server/metrics-server
 
